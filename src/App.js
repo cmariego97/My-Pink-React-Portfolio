63f9e030677d845bb9fe6f206323e7172b88b0a7
cmariego97 <cmarie.go97@gmail.com>
2022-10-29 11:14:44 -0400

    created header

--- src/App.js
+++ src/App.js
@@ -1,29 +1,96 @@
 import logo from './logo.svg';
 import './assets/css/App.css';
 
+//import MUI
+import { createTheme, ThemeProvider } from '@mui/system';
+
 //import components
 import Header from './components/Header';
+import NavBar from './components/NavBar'
+
+const theme = createTheme({
+  palette: {
+      primary: {
+          light: "#BE6B93",
+          main: "#CB2193",
+      },
+      secondary: {
+          main: "#9783C7",
+      },
+      error: {
+          main: "#f44336",
+      },
+      warning: {
+          main: "#ef742c"
+      },
+      info: {
+          main: "#271B36"
+      },
+      success: {
+          main: "#728464"
+      },
+      neutral: {
+          main: "#9CA6B5"
+      }
+  },
+  typography: {
+      fontFamily: [
+          'Nunito', 'sans-serif', 'Nunito Sans', 'Atma', 'cursive', 'Londrina Solid'
+      ],
+      h1: {
+          fontFamily: ['Abril Fatface'],
+          fontWeight: 400,
+          fontSize: 85,
+      },
+      h2: {
+          fontFamily: ['Poppins', 'sans serif'],
+          fontWeight: 600,
+          fontSize: 50,
+      },
+      h3: {
+          fontFamily: ['Poppins', 'sans serif'],
+          fontWeight: 400,
+          fontSize: 40,
+      },
+      h4: {
+          fontWeight: 400,
+          fontSize: 24,
+          textTransform: 'uppercase',
+          lineHeight: '2rem',
+      },
+      h5: {
+          fontFamily: ['Poppins', 'sans serif'],
+          fontWeight: 100,
+          fontSize: 18,
+          textTransform: 'uppercase',
+          lineHeight: '2rem',
+      },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-        <Header />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <NavBar />
+          <Header />
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </ThemeProvider>
+    
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,47 @@
-import React from 'react'
+import * as React from 'react';
+import '../assets/css/style.css'
+import pfp from '../assets/images/pfpAvatar.jpeg'
+
+// import MUI
+import { Avatar, Box, IconButton, Link, Typography} from '@mui/material'
+import { Stack } from '@mui/system';
+
+// import components
+import NavBar from './NavBar'
+
+// import icons
+import InstagramIcon from '@mui/icons-material/Instagram';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
+
 
 function Header() {
   return (
-    <div>Header</div>
+    <Box className='headWrapper'>
+        {/* AVATAR */}
+        {/* <Avatar alt="an image of clarence" src={pfp} sx={{ width: 24, height: 24 }}/> */}
+
+        <Box>
+            <Stack direction="row" spacing={4} className="socials">
+                <Link href="https://github.com/cmariego97">
+                    <GitHubIcon />
+                </Link>
+                <Link href="https://instagram.com/cme.gogogo">
+                    <InstagramIcon />
+                </Link>
+                <Link href="#">
+                    <FacebookRoundedIcon />
+                </Link>
+            </Stack>
+        </Box>
+        
+        {/* GREETING */}
+        <Box>
+            <Typography variant="h1" align="center" color="primary.main">Clarence Go</Typography>
+            <Typography variant="h5" align="right" color="info.main">Web Developer</Typography>
+        </Box>
+        
+    </Box>
   )
 }
 
